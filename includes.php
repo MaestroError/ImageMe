7845c04a4548871006559c6aaef7752cadefccfb
maestroerror <revaz.gh@gmail.com>
2021-09-29 13:09:04 +0400

    init

---
+++ includes.php
@@ -0,0 +1,6 @@
+<?php
+include "src/Traits/checkSystem.php";
+include "src/Traits/filesProcessing.php";
+include "src/Traits/staticHelpers.php";
+include "testfuncs.php";
+include "src/ImageMe.php";
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "includes.php";
+
+use maestroerror\ImageMe;
+
+$img = new ImageMe('irmakiPNG.png');
+
+// default upload
+$img->up();
+
+// upload with jpeg() or webp()
+$img->scale(400)->name("scaled")->jpeg()->up();
+
+// upload with force to webp with upW() (so jpeg() has no affect)
+$img->scale(50)->to("images/icons")->id("TEST123id")->jpeg()->upW();
+
+$img->test();
+
+
+
+// ImageMe::UPLOAD("irmaki.jpg");
